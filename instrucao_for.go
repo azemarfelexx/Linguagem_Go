9cc63679365b062979258c38749cfde36fe483b8
Azemar Felex <166390878+azemarfelexx@users.noreply.github.com>
2024-11-25 23:36:20 -0300

    Update instrucao_for.go
    Além de listar os números de 1 a 10, informo se são pares ou ímpares.

--- instrucao_for.go
+++ instrucao_for.go
@@ -5,6 +5,19 @@ import "fmt"
 func main() {
 
 	for i := 1; i <= 10; i++ {
+
 		fmt.Println(i)
+
+		if i%2 == 0 {
+
+			fmt.Println("Número Par")
+
+		} else {
+			fmt.Println("Número Ímpar")
+		}
+
 	}
 }
+
+// infromar se o número é par ou ímpar
+// resto é representado por %
